d7d4b6e924009229dd8d6dc2a4cf2c1c6547b595
Opilane TTHK <opilane@tthk.ee>
2021-02-16 11:22:54 +0200

    first commit

---
+++ RecipeDB/dbo.Table_2.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].RecipeIngredient
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [RecipeId] INT NOT NULL, 
+    [IngredientId] INT NOT NULL, 
+    CONSTRAINT RecipeIdFK FOREIGN KEY (RecipeId) REFERENCES Recipe(Id), 
+    CONSTRAINT IngredientIdKF FOREIGN KEY (IngredientId) REFERENCES Ingredient(Id) 
+)
